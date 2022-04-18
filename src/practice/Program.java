dde13e4538b6366796d399d5d8ec96a9e18e8e91
tc0525 <dfhdfhf_19751204new>
2022-04-18 20:55:05 +0300

    18.04

--- src/practice/Program.java
+++ src/practice/Program.java
@@ -1,5 +1,6 @@
 package practice;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Program {
@@ -15,23 +16,99 @@ public class Program {
 //        char ch=102 + 1;
 //        char ch= 'a';
 
-        int a = 6;
-        int b = 4;
-        if(a==b){
-            System.out.println("если переменные равны");
-        }
-        else if(a<b){
-            System.out.println("если переменная a меньше b");
-        }
-        else if(a>b){
-            System.out.println("если переменная b меньше a");
-        }
-        if((a+b) % 2 == 0){
-            System.out.println("maybe a and b are even");
-        }
-        else{
-            System.out.println("some variable is odd");
+//        int a = 6;
+//        int b = 4;
+//        if(a==b){
+//            System.out.println("если переменные равны");
+//        }
+//        else if(a<b){
+//            System.out.println("если переменная a меньше b");
+//        }
+//        else if(a>b){
+//            System.out.println("если переменная b меньше a");
+//        }
+//        if((a+b) % 2 == 0){
+//            System.out.println("maybe a and b are even");
+//        }
+//        else{
+//            System.out.println("some variable is odd");
+//        }
+//
+//
+//
+//        for (int i = 0; i < 10; i++){
+//            if (i == 5) {
+//                continue;
+//            }
+//            System.out.println( i );
+////        }
+//        String str = "elephant";
+//        for (int i = 0; i < 10; i++) {
+////            if (i == 5) {
+//                continue;
+//            }
+//            System.out.println(str.charAt(i));
+//        }
+//
+//        int j = 6;
+//        while (j < 20) {
+//
+//            System.out.println(j);
+//            j++;
+//        }
+//        int j = - 7;
+//        do{
+//            System.out.println(j);
+//            j++;
+//        }
+//        while (j <= 0);
+//        int [] nums = new int[4];
+//
+//        nums[0] = 1;
+//        nums[1] = 2;
+//        nums[2] = 4;
+//        nums[3] = 100;
+//        System.out.println(nums[3]);
+
+
+
+
+  //      int[] array = {1, 2, 3, 5, 7, 9};
+        int [] array = new int[] {9, 2, 6, 4, 5, 12, 7, 8, 6};
+            int sum = 0;
+            for (int i = 0; i < array.length; i++) {
+                sum = sum + array[i];
+            }
+            System.out.println(sum);
         }
 
-    }
-}
+//            array[i] = array[i]
+        }
+
+
+//        int[] nums1 = new int[] { 8, 23, 70, 46, 25, 85 };
+//        for (int i = 0; i < nums1.length; i++) {
+//            nums1[i] = nums1[i] * 2;
+//            System.out.println(nums1[i]);
+
+
+       //     System.out.print(nums1[i] + " ");
+      //      System.out.println(nums1[i]);
+
+
+
+//        int[][] nums2 = { { 0, 111, 2 }, { 3, 4, 5, 40 } };
+//
+//        System.out.println(nums2[0][1]);
+//        String [] vegs = {"cucumber", "carrot", "potato"};
+//        System.out.println(vegs[0]);
+
+//        int length = nums.length;
+//        int last = nums[nums.length - 2];
+//        System.out.println(Arrays.toString(nums));
+//        int[] nums2 = nums;
+ //       nums2[0] = 88;
+//        System.out.println(Arrays.toString(nums));
+//        System.out.println(nums2);
+
+
