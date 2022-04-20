1f538e0f57ebe19258b86b6e61c57178eff52611
tc0525 <dfhdfhf_19751204new>
2022-04-20 21:24:10 +0300

    20.04

---
+++ src/test/CodewarsTS.java
@@ -0,0 +1,15 @@
+package test;
+
+public class CodewarsTS {
+    public static void main(String[] args) {
+
+        System.out.println(doubleInteger (5));
+
+    }
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        i = i * 2;
+
+        return i;
+    }
+}
